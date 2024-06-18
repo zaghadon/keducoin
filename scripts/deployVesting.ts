d8ecca7a0741267ed578c6023aeca5b1462fb6bc
Nzagha David Eze <zaghadon@gmail.com>
2024-06-18 01:17:17 +0100

    Updated Deployment Script to reflect Contract Changes

--- scripts/deployVesting.ts
+++ scripts/deployVesting.ts
@@ -14,10 +14,10 @@ async function main() {
   });
 
   const sedfbeneficiary = process.env.SEDF_BENEFICIARY as string;
-  const start = Math.floor(Date.now() / 1000); // current time in seconds
-  const duration = 10 * 365 * 24 * 60 * 60; // 10 years in seconds
+  //const start = 1717023600; //Thursday May 30 2024 00:00:00 GMT+0100 (West Africa Standard Time)
+  //const duration = 10 * 365 * 24 * 60 * 60; // 10 years in seconds (315360000)
 
-  const instance = await ContractFactory.deploy(sedfbeneficiary, start, duration);
+  const instance = await ContractFactory.deploy(sedfbeneficiary);
   await instance.waitForDeployment();
 
   console.log(`Contract deployed to ${await instance.getAddress()}`);
